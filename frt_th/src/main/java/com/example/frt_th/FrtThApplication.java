479b54cdb406597a64e2144b9c95536526202e07
Richar Silva <richard.silva@nextcode.cl>
2024-05-08 22:18:50 +0200

    Se agrega el proyecto Semilla

---
+++ frt_th/src/main/java/com/example/frt_th/FrtThApplication.java
@@ -0,0 +1,13 @@
+package com.example.frt_th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrtThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrtThApplication.class, args);
+	}
+
+}
---
+++ frt_th/src/test/java/com/example/frt_th/FrtThApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frt_th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrtThApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
