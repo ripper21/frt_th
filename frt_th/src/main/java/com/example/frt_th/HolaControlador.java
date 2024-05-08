bef8c457cd4c2e994d8df8d69776db5711a55956
Richar Silva <richard.silva@nextcode.cl>
2024-05-08 23:03:43 +0200

    Still playing

--- frt_th/src/main/java/com/example/frt_th/HolaControlador.java
+++ frt_th/src/main/java/com/example/frt_th/HolaControlador.java
@@ -2,13 +2,18 @@ package com.example.frt_th;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.model.IAttribute;
 
 @Controller
 public class HolaControlador {
 
     @GetMapping("/hola")
-    public String hola(){
+    public String hola(@RequestParam(name = "nombre", required = false, defaultValue = "Mundo") String nombre, Model model) {
+        model.addAttribute("nombre", nombre);
         return "hola";
     }
+
 }
