cdb1f598ba302504e1b886ba349da18b5c384749
Richar Silva <richard.silva@nextcode.cl>
2024-05-21 23:04:51 +0200

    agregar funcionalidades

--- src/main/java/com/registro/usuarios/service/UsuarioServiceImpl.java
+++ src/main/java/com/registro/usuarios/service/UsuarioServiceImpl.java
@@ -1,35 +1,75 @@
 package com.registro.usuarios.service;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.registro.usuarios.dto.UsuarioRegistroDTO;
 import com.registro.usuarios.modelo.Rol;
 import com.registro.usuarios.modelo.Usuario;
 import com.registro.usuarios.repository.UsuarioRepositorio;
+import com.registro.usuarios.seguridad.SecurityConfiguration;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
 	
 	private UsuarioRepositorio usuarioRepository;
 	
-	
 
+    private BCryptPasswordEncoder passwordEncoder;
+
+
+	@Autowired
+    public UsuarioServiceImpl(@Lazy SecurityConfiguration securityConfiguration) {
+    }
+
+    @Autowired
+    public void setPasswordEncoder(BCryptPasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
     public UsuarioServiceImpl(UsuarioRepositorio usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
+    
+    
+	@Override
+	public Usuario guardar(UsuarioRegistroDTO registroDTO) {
+		Usuario usuario = new Usuario(registroDTO.getNombre(), 
+				registroDTO.getApellido(),registroDTO.getEmail(),
+				passwordEncoder.encode(registroDTO.getPassword()),Arrays.asList(new Rol("ROLE_USER")));
+		return usuarioRepository.save(usuario);
+	}
 
 
 	@Override
-	public Usuario guardar(UsuarioRegistroDTO registroDTO) {
-		Usuario usuario = new Usuario(registroDTO.getNombre(),
-				registroDTO.getApellido(),
-				registroDTO.getEmail(),
-				registroDTO.getPassword(),
-				Arrays.asList(new Rol ("ROLE_USER")));				
-				
-		return usuarioRepository.save(usuario);
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = usuarioRepository.findByEmail(username);
+		if(usuario == null) {
+			throw new UsernameNotFoundException("Usuario o password inválidos");
+		}
+		return new User(usuario.getEmail(),usuario.getPassword(), mapearAutoridadesRoles(usuario.getRoles()));
 	}
+	
+	private Collection<? extends GrantedAuthority> mapearAutoridadesRoles(Collection<Rol> roles){
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getNombre())).collect(Collectors.toList());
+	}
+	
+	@Override
+	public List<Usuario> listarUsuarios() {
+		return usuarioRepository.findAll();
+	}	
+
+	
 
 }
